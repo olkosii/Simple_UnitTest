b06b5a5f1d22c90374ffe3d09072eff0e50eb1ff
Olexandr Kosii <okosiy14@gmail.com>
2022-03-28 12:32:56 +0300

    Simple Test

---
+++ UnitTest/UnitTest_Practise.Test/Sum_Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UnitTest_Practise.Test
+{
+    [TestFixture]
+    public class Sum_Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = Sum.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+        
+    }
+}
---
+++ UnitTest/UnitTest_Practise/Sum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTest_Practise
+{
+    public class Sum
+    {
+        public static int Add(int first,int second)
+        {
+            return first + second;
+        }
+    }
+}
